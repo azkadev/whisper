b545fe10c11f24c2705071968dc5548a3e3addef
Your Name <you@example.com>
2022-10-25 15:34:56 +0700

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,8 +17,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
