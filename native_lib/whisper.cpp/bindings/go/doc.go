168f4083e002c6ec245eaae3fb27edbcaea93c6c
￴ <>
2023-02-06 02:28:11 +0700

    update

--- native_lib/whisper.cpp/bindings/go/doc.go
+++
@@ -1,5 +0,0 @@
-/*
-github.com/ggerganov/whisper.cpp/bindings/go
-provides a speech-to-text service bindings for the Go programming language.
-*/
-package whisper
--- native_lib/whisper.cpp/bindings/go/pkg/whisper/doc.go
+++
@@ -1,4 +0,0 @@
-/*
-This is the higher-level speech-to-text whisper.cpp API for go
-*/
-package whisper
