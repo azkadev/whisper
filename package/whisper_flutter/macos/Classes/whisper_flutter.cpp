6684d8baa822d56a91d83be03ae8959d513fbbbb
￴ <>
2023-03-07 06:55:12 +0700

    test fix ios and macos

--- package/whisper_flutter/macos/Classes/whisper_flutter.cpp
+++ package/whisper_flutter/macos/Classes/whisper_flutter.cpp
@@ -1,3 +1,5 @@
 // Relative import to be able to reuse the C sources.
 // See the comment in ../{projectName}}.podspec for more information.
-#include "../../src/whisper_flutter.c"
+// #include "../../src/whisper_flutter.c"
+
+#include "../../src/main.cpp"
